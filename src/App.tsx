811290b8b94bf7ef626c0de1d613f2f38f27b634
stalugaexpert <stalegapawel@gmail.com>
2022-05-22 13:41:53 +0200

    added configuration to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
-import React from "react";
-import "./App.css";
-
-function App() {
-  return <div className="App"></div>;
+function App(): JSX.Element {
+  return <div />;
 }
 
 export default App;
